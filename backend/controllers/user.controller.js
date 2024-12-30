ee061cffdece43c38c6c335f3edee9095bf099b7
Danny <dnioko0412@gmail.com>
2024-12-30 17:24:51 +0800

    first

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -11,7 +11,7 @@ export const register = async (req, res) => {
         if (!fullname || !email || !phoneNumber || !password || !role) {
             return res.status(400).json({
                 message: "Something is missing",
-                success: false
+                success: false  
             });
         };
         const file = req.file;
